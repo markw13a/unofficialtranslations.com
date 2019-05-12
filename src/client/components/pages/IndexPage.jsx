43934afd6e23611c95d44f95561d382f3e33f037
mark <markw13a@gmail.com>
2019-05-12 18:14:08 +0100

    Article list should now basically work

--- src/client/components/pages/IndexPage.jsx
+++ src/client/components/pages/IndexPage.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import axios from 'axios';
 import NavBar from '../common/NavBar';
 import Footer from '../common/Footer';
 
@@ -8,27 +7,23 @@ class IndexPage extends React.Component {
         super(props);
 
         this.state = {
-            data: {
-                value: null,
-            }
         };
     };
 
     // retrieve article data from back-end
     componentDidMount() {
-        axios.get('/rest/get')
-            .then(value => {
-                this.setState({data: {value: value.data.data}});
+        fetch('/rest/get')
+            .then(res => res.json())
+            .then(json => {
+                this.setState({data: json.data});
             });
     };
 
     render() {
-        let {value} = this.state.data;
-
         return (
             <div className="IndexPage Page"> 
                 <NavBar />
-                <ArticlesList articles={value} />
+                <ArticlesList data={this.state.data} />
                 <Footer />
             </div>
         );
@@ -38,32 +33,41 @@ class IndexPage extends React.Component {
 /**
  * @param articles array of json objects containing info on articles
  */
-const ArticlesList = ({articles}) => {
-    if( !articles ) return null;
-
+const ArticlesList = ({data}) => {
+    if( !data ) return null;
+    console.warn(data);
     // want to display newest > oldest
-    articles.reverse();
+    data.reverse();
 
     // Put the stored date string in to a more human-readable form
     const  dateOptions = {year: 'numeric', month: 'long', day: 'numeric' };
 
     return (
         <div className="container">
-            {articles.map( article => (
-                <div className="article">
-                    <h2 className="title">{article.targetLanguageTitle}</h2>
-                    <div className="imgContainer"><img src={article.image} /></div>
-                    <div className="blurb">{article.blurb}</div>
-                    <div className="other">
-                        <div className="extraInfo">
-                            <small className="date">{new Date(article.date).toLocaleDateString("en-UK", dateOptions)}</small>
-                        </div>
-                        <div className="readMore cta">
-                            <a href={'/article/' + article.endpointTitle}>Read More</a>
+            {data.map( article => {
+                // Just show English version on front-page
+                // TODO: Make language something that can just be toggled globally
+                const enArticle = article.articles.find( article => article.language === 'en');
+
+                if( !enArticle ) enArticle = article.articles[0];
+
+                const {title, image, id, blurb, date} = enArticle;
+
+                return (
+                    <div className="article">
+                        <h2 className="title">{title}</h2>
+                        <div className="imgContainer"><img src={image} /></div>
+                        <div className="blurb">{blurb}</div>
+                        <div className="other">
+                            <div className="extraInfo">
+                                <small className="date">{new Date(date).toLocaleDateString("en-UK", dateOptions)}</small>
+                            </div>
+                            <div className="readMore cta">
+                                <a href={'/article/' + id}>Read More</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-            ))}
+            )})}
         </div>
     );
 };
